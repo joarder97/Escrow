3d8a266e90ce7c6fd6df1af52a153c20062e4f64
joarder97 <masudul.pallab99@gmail.com>
2021-09-30 01:42:10 +0600

    buyer

--- frontend-buyer/pages/cancelOrder.js
+++ frontend-buyer/pages/cancelOrder.js
@@ -1,9 +1,52 @@
 import styles from '../styles/Home.module.css'
+import { Alert, Button, Space } from 'antd';
 
-export default function Home() {
+
+
+export default function cancelOrder() {
+
+  const onSubmit = async () => {
+
+    let key = 'o1';
+
+    const url = 'http://localhost:3000/cancelOrder';
+
+    let requestBody = {
+      key:key,
+    };
+
+    let response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+  });
+    let data = await response.json();
+    console.log(data);
+
+    
+};
+
   return (
-    <div className={styles.container}>
-        cancel Order
-    </div>
+    <Alert
+    message="Cancel Order"
+    description="Do you want to cancel your order?"
+    type="warning"
+    action={
+      <Space direction="vertical">
+        <Button size="small" danger type="ghost" onClick={onSubmit}>
+          Yes
+        </Button>
+        <Button size="small" type="primary">
+          No
+        </Button>
+      </Space>
+    }
+    closable
+  />
   )
 }
